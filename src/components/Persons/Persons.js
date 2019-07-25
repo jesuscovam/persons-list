2d886ce3e3ccacec9cc7d6c54098984fd22d7b15
System Administrator <root@JESUSs-MacBook-Pro.local>
2019-07-25 18:38:45 -0500

    Added functionality to shouldComponentUpdate hook so it re-renders the component only when the props change

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,7 +13,11 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[Persons.js] shouldComponentUpdate");
-    return true;
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
@@ -24,6 +28,10 @@ class Persons extends Component {
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log("[Persons.js] componentDidUpdate");
     console.log(snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log("[Persons.js] componentWillUnmount");
   }
 
   render() {
